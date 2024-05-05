1c94f6251d2eea33f75ccc3d1e0fd71fc4736f91
nnsnico <yuji.jampan.blue@gmail.com>
2024-05-05 21:28:18 +0900

    feat: create command using cli module

--- adv.vsh
+++ adv.vsh
@@ -3,15 +3,52 @@
 module main
 
 import cmd
+import cli
+import os
+import utils
+import v.vmod
 
-adb := cmd.create_adb() or {
-	eprintln(err)
+const manifest = vmod.decode(@VMOD_FILE) or { panic(err) }
+
+@[noreturn]
+fn print_err(msg IError) {
+	eprintln(utils.response_err(msg.str()))
 	exit(1)
 }
 
-devices := adb.select_active_device() or {
-	eprintln(err)
-	exit(1)
+mut app := cli.Command{
+	name: manifest.name
+	description: manifest.description
+	version: manifest.version
+	posix_mode: true
+	defaults: struct {
+		man: false
+	}
 }
 
-println(devices)
+app.add_command(cli.Command{
+	name: 'devices'
+	description: 'list connected devices'
+	execute: fn (_ cli.Command) ! {
+		adb := cmd.create_adb() or { print_err(err) }
+		all_devices := adb.get_all_active_devices() or { print_err(err) }
+
+		for d in all_devices {
+			println(d.name)
+		}
+	}
+})
+
+app.add_command(cli.Command{
+	name: 'device'
+	description: 'select a device from connected device list'
+	execute: fn (_ cli.Command) ! {
+		adb := cmd.create_adb() or { print_err(err) }
+		selected_device := adb.select_active_device() or { print_err(err) }
+
+		println(selected_device.name)
+	}
+})
+
+app.setup()
+app.parse(os.args)
--- cmd/devices.v
+++ cmd/devices.v
@@ -5,15 +5,18 @@ import android
 import io.util { temp_file }
 
 pub fn (a Adb) get_all_active_devices() ![]android.Device {
-	devices := a.get_devices_str()!.map(fn (s string) []string {
-		return s.split('\t')
-	})
-	return devices.map(fn (s []string) android.Device {
+	devices := a.get_devices_str()!.map(fn (s string) android.Device {
+		device_info := s.split('\t')
 		return android.Device{
-			name: s[0]
-			device_type: android.check_device_type(s[0])
+			name: device_info[0]
+			device_type: android.check_device_type(device_info[0])
 		}
 	})
+	return if devices.len != 0 {
+		devices
+	} else {
+		error('No connected Device')
+	}
 }
 
 pub fn (a Adb) select_active_device() !android.Device {
@@ -28,26 +31,18 @@ pub fn (a Adb) select_active_device() !android.Device {
 
 	os.write_file(input_file, device_str.join('\n'))!
 
-	os.system('${fzf} -0 < "${input_file}" > "${output_file}"')
+	os.system('${fzf} -0 -1 --preview= < "${input_file}" > "${output_file}"')
 
-	selected_line := os.read_lines(output_file)!.map(fn (s string) []string {
-		return s.split('\t')
-	})
-	devices := selected_line.map(fn (s []string) android.Device {
-		return android.Device{
-			name: s[0]
-			device_type: android.check_device_type(s[0])
-		}
-	})
+	selected_line := os.read_file(output_file)!.split('\t')
+	device := android.Device{
+		name: selected_line[0]
+		device_type: android.check_device_type(selected_line[0])
+	}
 
 	os.rm(input_file)!
 	os.rm(output_file)!
 
-	return if devices.len != 0 {
-		devices[0]
-	} else {
-		error('No connected Device')
-	}
+	return device
 }
 
 fn (a Adb) get_devices_str() ![]string {
@@ -55,7 +50,7 @@ fn (a Adb) get_devices_str() ![]string {
 	mut devices := devices_str.split('\n')
 	devices.drop(1)
 
-	if devices.len != 0 && devices[0].contains("* daemon started successfully") {
+	if devices.len != 0 && devices[0].contains('* daemon started successfully') {
 		devices.clear()
 		return a.get_devices_str()!
 	} else {
